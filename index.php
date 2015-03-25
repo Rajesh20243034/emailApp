4338f030dc94de97f20b2610cca5c1b74c8e565d
Rajesh20243034 <rajesh.patelp3034@gmail.com>
2015-03-25 10:31:28 +0530

    Basic boilerplating

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script  src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.6/angular.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css"/>
+    <style type="text/css">
+      .container {
+          margin-top: 20px;
+      }
+    </style>
+</head>
+<body>
+    <div class="container">
+    </div>
+</body>
+</html>
